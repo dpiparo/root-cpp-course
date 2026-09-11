06551a10d4fb6baa0abf81ad3134c18d2434a776
Vincenzo Eduardo Padulano <vincenzo.eduardo.padulano@cern.ch>
2026-09-11 17:37:07 +0200

    Add interactive graphics directory

---
+++ course/basic/graphics/README.md
@@ -0,0 +1,5 @@
+This directory contains examples using ROOT graphics interactively, i.e. creating a canvas and visualising data. Run the examples as:
+
+```
+$: root -l <example_basename>.cpp
+```
---
+++ course/basic/graphics/example_01_interactive_canvas.cpp
@@ -0,0 +1,21 @@
+#include <TCanvas.h>
+#include <TH1.h>
+
+#include <memory>
+
+// We create a unique_ptr to a canvas at global scope to avoid losing the canvas
+// object after the end of the main function
+auto canvas = std::make_unique<TCanvas>("c", "c", 800, 600);
+
+void example_01_interactive_canvas() {
+  TH1D h{"h", "h", 10, -5, 5};
+  h.FillRandom("gaus");
+
+  // We call DrawClone which creates a clone of the histogram and attaches that
+  // to the canvas. This way, even after we leave this function scope, the
+  // canvas still holds the histogram that has been drawn on it.
+  h.DrawClone();
+  canvas->Draw();
+}
+
+int main() { example_01_interactive_canvas(); }
