744f6a419db67541b5175fc0da8271d72a62f19f
Danilo Piparo <danilo.piparo@cern.ch>
2026-09-14 14:01:18 +0200

    Format 02-02 solutions

--- course/02-io/02-io-custom-classes/solutions/meta_example.C
+++ course/02-io/02-io-custom-classes/solutions/meta_example.C
@@ -1,7 +1,8 @@
-void meta_example(){
-    auto c = TClass::GetClass("myVector");
-    auto dataMembers = c->GetListOfDataMembers();
-    auto m = dynamic_cast<TDataMember*>(dataMembers->FindObject("m_mag"));
-    bool isPersistent = m->IsPersistent();
-    cout << "Data member " << m->GetName() << " of class " << c->GetName() << " is " << (isPersistent ? "not " : "") << "transient." << endl;
+void meta_example() {
+  auto c = TClass::GetClass("myVector");
+  auto dataMembers = c->GetListOfDataMembers();
+  auto m = dynamic_cast<TDataMember *>(dataMembers->FindObject("m_mag"));
+  bool isPersistent = m->IsPersistent();
+  cout << "Data member " << m->GetName() << " of class " << c->GetName()
+       << " is " << (isPersistent ? "not " : "") << "transient." << endl;
 }
--- course/02-io/02-io-custom-classes/solutions/write.C
+++ course/02-io/02-io-custom-classes/solutions/write.C
@@ -1,11 +1,11 @@
 void write() {
-    
-    myVector v(1, 2, 3);
-    std::vector<myVector> vs{{1,2,3},{4,5,6},{7,8,9}};
 
-    auto file = std::unique_ptr<TFile>(TFile::Open("myVector_file.root", "RECREATE"));
-    file->WriteObject(&v, "v");
-    file->WriteObject(&vs, "vs");
-    file->Close();
+  myVector v(1, 2, 3);
+  std::vector<myVector> vs{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 
+  auto file =
+      std::unique_ptr<TFile>(TFile::Open("myVector_file.root", "RECREATE"));
+  file->WriteObject(&v, "v");
+  file->WriteObject(&vs, "vs");
+  file->Close();
 }
