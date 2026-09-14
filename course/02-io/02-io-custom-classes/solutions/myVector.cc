c5f2490c043fd5e75e6222219eae6b4febcbe30c
Danilo Piparo <danilo.piparo@cern.ch>
2026-09-14 14:01:18 +0200

    Format 02-02 solutions

--- course/02-io/02-io-custom-classes/solutions/myVector.cc
+++ course/02-io/02-io-custom-classes/solutions/myVector.cc
@@ -2,11 +2,7 @@
 
 #include <iostream>
 
-void myVector::Print()
-{
-   std::cout << "x = " << m_x 
-             << " y = " << m_y 
-             << " z = " << m_z 
-             << " mag = " << m_mag 
-             << std::endl;
+void myVector::Print() {
+  std::cout << "x = " << m_x << " y = " << m_y << " z = " << m_z
+            << " mag = " << m_mag << std::endl;
 }
